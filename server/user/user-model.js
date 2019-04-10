8221bb67a3f8f1f61de5fc1fd3fa40a48a2a621a
Rajendra sahoo <rajendrasahoodbpb@gmail.com>
2019-04-10 11:09:58 +0530

    - minor update

--- server/user/user-model.js
+++ server/user/user-model.js
@@ -3,19 +3,21 @@
 var Schema = mongoose.Schema;
 var constants = require("../../config/constants");
 var userSchema = new global.mongoose.Schema({
+    //basic
     role: { type: Schema.Types.ObjectId, ref: 'role' },
     mobile: { type: String,unique:true,required:true ,minlength:10,maxlength:10},
-    username: { type: String },
-    password: { type: String },
     // child details
+    firstName: { type: String },
+    lastName: { type: String },
     age: { type: Number },
     gender: { type: String,enum: ['male','female'], },
     school: { type: String },
     grade: { type: String },
     
+    //admin
+    username: { type: String },
+    password: { type: String },
     email: { type: String },
-    firstName: { type: String },
-    lastName: { type: String },
     isDelete:{type:Boolean,default:false}
 });
 
